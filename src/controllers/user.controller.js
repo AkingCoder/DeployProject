baa74d0e826707adea884cd7f5157a5e1e52eaac
AkingCoder <162672866+AkingCoder@users.noreply.github.com>
2024-12-20 23:19:57 +0530

    Update user.controller.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -112,7 +112,7 @@ const logInUser = asyncHandler(async (req, res) => {
 
     const options = {
         httpOnly: true,
-        secure: false,
+        secure: true,
         sameSite: "None",
     }
 
